a5800dda645f9387a97201986b65c9f8ce8129c4
nikisetti01 <n.settimelli@studenti.unipi.it>
2024-05-20 22:50:13 +0200

    actuator in thermo make get for lpg and thermo

--- coap-network/sensors/thermometer/actuator.c
+++ coap-network/sensors/thermometer/actuator.c
@@ -18,7 +18,8 @@
 #define LOG_MODULE "App"
 #define LOG_LEVEL  LOG_LEVEL_APP
 
-#define SERVER_EP "coap://[fd00::202:2:2:2]:5683"
+#define SERVER_EP_TEMP "coap://[fd00::202:2:2:2]:5683"
+#define SERVER_EP_LPG   "coap://[fd00::204:4:4:4]:5683"
 
 static char* service_url = "predict-temp";
 
@@ -39,11 +40,13 @@ PROCESS_THREAD(coap_client_process, ev, data)
 {
     PROCESS_BEGIN();
 
-    static coap_endpoint_t server_ep;
+    static coap_endpoint_t server_ep_temp;
+    static coap_endpoint_t server_ep_lpg;
     static coap_message_t request[1];
     static struct etimer timer;
 
-    coap_endpoint_parse(SERVER_EP, strlen(SERVER_EP), &server_ep);
+    coap_endpoint_parse(SERVER_EP_TEMP, strlen(SERVER_EP_TEMP), &server_ep_temp);
+    coap_endpoint_parse(SERVER_EP_LPG,strlen(SERVER_EP_LPG),&server_ep_lpg);
 
     etimer_set(&timer, CLOCK_SECOND * 5);
 
@@ -53,9 +56,16 @@ PROCESS_THREAD(coap_client_process, ev, data)
         coap_init_message(request, COAP_TYPE_CON, COAP_GET, 0);
         coap_set_header_uri_path(request, service_url);
 
-        printf("Sending request to %s\n", SERVER_EP);
+        printf("Sending request to %s\n", SERVER_EP_TEMP);
 
-        COAP_BLOCKING_REQUEST(&server_ep, request, response_handler);
+        COAP_BLOCKING_REQUEST(&server_ep_temp, request, response_handler);
+        coap_init_message(request, COAP_TYPE_CON, COAP_GET, 0);
+        coap_set_header_uri_path(request, "/res_danger");
+        printf("Sending request to %s\n", SERVER_EP_LPG);
+        COAP_BLOCKING_REQUEST(&server_ep_lpg, request, response_handler);
+
+            // Reset the timer for the next cycle
+            etimer_reset(&timer);
 
         etimer_reset(&timer);
     }
