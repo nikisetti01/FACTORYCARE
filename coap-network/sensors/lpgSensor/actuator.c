bf2e3c869908b6d3ef841a68cd8d269a9a97280f
iot_ubuntu_intel <iot_ubuntu_intel@iot-ubuntu-intel.myguest.virtualbox.org>
2024-05-23 14:57:23 +0200

    roba

--- coap-network/sensors/lpgSensor/actuator.c
+++ coap-network/sensors/lpgSensor/actuator.c
@@ -20,12 +20,12 @@
 #define LOG_LEVEL  LOG_LEVEL_APP
 
 #define SERVER_EP_TEMP "coap://[fd00::202:2:2:2]:5683"
-#define SERVER_EP_LPG  "coap://[fe80::f6ce:3686:4ff2:1a3]:5683" //TRY WITH DONGLE
+#define SERVER_EP_LPG  "coap://[fe80::f6ce:36f4:9606:f869]:5683" //TRY WITH DONGLE
 //#define SERVER_EP_LPG   "coap://[fd00::203:3:3:3]:5683"
 
 #define CRITICAL_TEMP_VALUE 20
 
-static char* service_url = "predict-temp";
+//static char* service_url = "predict-temp";
 
 PROCESS(coap_client_process, "CoAP Client Process");
 AUTOSTART_PROCESSES(&coap_client_process);
@@ -39,6 +39,11 @@ void response_handler_LPG(coap_message_t *response) {
         printf("No response received.\n");
         return;
     }
+      leds_single_off(LEDS_GREEN);
+      leds_single_off(LEDS_RED);
+      leds_single_off(LEDS_YELLOW);
+      leds_single_off(LEDS_BLUE);
+
   const uint8_t *chunk;
 
   int len = coap_get_payload(response, &chunk);
@@ -49,12 +54,17 @@ void response_handler_LPG(coap_message_t *response) {
   if(lpgValue == 0)
   {
     printf("lpg is normal\n");
+    leds_single_on(LEDS_GREEN);
+    return;
   }else if(lpgValue == 1)
   {
     printf("lpg is dangerous\n");
+    leds_single_on(LEDS_RED);
+    return;
   }else if(lpgValue == 2)
   {
     printf("lpg is critical\n");  
+
   }else
   {
     printf("lpg is unknown\n");
@@ -97,17 +107,19 @@ PROCESS_THREAD(coap_client_process, ev, data)
     coap_endpoint_parse(SERVER_EP_TEMP, strlen(SERVER_EP_TEMP), &server_ep_temp);
     coap_endpoint_parse(SERVER_EP_LPG,strlen(SERVER_EP_LPG),&server_ep_lpg);
 
-    etimer_set(&timer, CLOCK_SECOND/4); // * 5 è assai lento
+    etimer_set(&timer, CLOCK_SECOND); // * 5 è assai lento
     static struct etimer lpg_timer;
 
     while(1) {
+      printf("Starting the client process\n");
       PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
 
       //first request to the temperature sensor
-      coap_init_message(request, COAP_TYPE_CON, COAP_GET, 0);
+      /*coap_init_message(request, COAP_TYPE_CON, COAP_GET, 0);
       coap_set_header_uri_path(request, service_url);
       printf("Sending request to %s\n", SERVER_EP_TEMP);
       COAP_BLOCKING_REQUEST(&server_ep_temp, request, response_handler_TEMP); //modifica creando risposta per temp e lpg in modo distinto
+      */
 
       //second request to the lpg sensor
       coap_init_message(request, COAP_TYPE_CON, COAP_GET, 0);
@@ -118,26 +130,22 @@ PROCESS_THREAD(coap_client_process, ev, data)
       // Reset the timer for the next cycle
       etimer_reset(&timer);
 
-      if(lpgValue == 0) {
-         leds_single_on(LEDS_GREEN);
+      printf("lpgValue: %d\n", lpgValue);
+      if(lpgValue == 0) {//PROVO A INVERTIRE, CORRETTAMENTE NON VA
+         printf("SETTING LED GREEN\n");
       }
       
       if(lpgValue == 1) {
-        leds_single_on(LEDS_RED);
-        leds_single_off(LEDS_NUM_TO_MASK(LEDS_GREEN));
+        printf("SETTING LED RED\n");
       }
 
       if(lpgValue == 2) {
-        etimer_set(&lpg_timer, CLOCK_SECOND*2);
-        leds_single_on(LEDS_RED);
+        etimer_set(&lpg_timer, CLOCK_SECOND);
+        leds_single_toggle(LEDS_RED);
+        printf("SETTING LED RED PULSING\n");
       }
 
       PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&lpg_timer));
-
-      // Toggle the LED
-      if(lpgValue == 2) {
-        leds_single_toggle(LEDS_NUM_TO_MASK(LEDS_RED));
-      }
 
       if(tempValue>CRITICAL_TEMP_VALUE){
         leds_on(LEDS_NUM_TO_MASK(LEDS_YELLOW));
