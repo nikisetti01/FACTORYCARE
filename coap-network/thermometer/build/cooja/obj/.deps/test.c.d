a675bc7b97f36051f5c68c1b36df4422d6c40b72
nikisetti01 <n.settimelli@studenti.unipi.it>
2024-05-25 17:15:58 +0200

    post of client resolving problem of parse with server

--- coap-network/thermometer/build/cooja/obj/.deps/test.c.d
+++
@@ -1,5 +0,0 @@
-build/cooja/obj/test.o: ../cJSON-master/test.c \
- ../../../../os/net/app-layer/coap/module-macros.h \
- ../cJSON-master/cJSON.h
-../../../../os/net/app-layer/coap/module-macros.h:
-../cJSON-master/cJSON.h:
