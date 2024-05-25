a8a85c3b62258eba63793d7f148d6fb4b1d3aadd
nikisetti01 <n.settimelli@studenti.unipi.it>
2024-05-25 17:23:32 +0200

    resolving merge

--- java/factorycare/src/main/java/com/unipi/dii/iot/main.java
+++ java/factorycare/src/main/java/com/unipi/dii/iot/main.java
@@ -3,15 +3,9 @@ package com.unipi.dii.iot;
 public class main {
     public static void main(String args[]) {
         MyServer server = new MyServer();
-<<<<<<< HEAD
-       // server.add(new CoAPResourceExample("hello"));
-       server.add(new CoapResourceRegistrationSensor("sensor_registration"));
-
-=======
         server.add(new CoAPResourceExample("hello"));
         server.add(new CoapResourceRegistrationSensor("registrationSensor"));
         server.add(new CoapResourceRegistrationActuator("registrationActuator"));
->>>>>>> d6acee9 (added all the modifies for server and java app with leds for registration)
         server.start();
         System.out.print("The server is Running!");
     }
