cfc92b221c90f89342e708e7505dcf4db4ef030d
MenchiniDev <lorenzomenchini42@gmail.com>
2024-05-28 21:03:40 +0200

    resolved bug in handlepost for sensors

--- java/factorycare/src/main/java/com/unipi/dii/iot/CoapResourceRegistrationSensor.java
+++ java/factorycare/src/main/java/com/unipi/dii/iot/CoapResourceRegistrationSensor.java
@@ -10,7 +10,6 @@ import org.eclipse.californium.core.server.resources.CoapExchange;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 class CoapResourceRegistrationSensor extends CoapResource {
 
@@ -32,85 +31,60 @@ class CoapResourceRegistrationSensor extends CoapResource {
         //db setup
         
         String payloadString = exchange.getRequestText();
-        String ipAddress=exchange.getSourceAddress().getHostAddress();
+        String ipAddress=exchange.getSourceAddress().getHostAddress();        
         System.out.println("Payload received: " + payloadString+ " \nlunghezza: "+ payloadString.length());
         System.out.println("IP address: " + ipAddress + "\n");
-        
         JSONObject json = null;
         try {
-            JSONParser parser = new JSONParser();
-            json = (JSONObject) parser.parse(payloadString);
-            System.out.println("Parsed JSON: " + json);
-            } catch (ParseException err) {
-                System.err.println("JSON parsing error: " + err.getMessage());
-                exchange.respond(CoAP.ResponseCode.BAD_REQUEST, "Invalid JSON format");
-                return;
-            }
-            Response response;
-            // Extracting and handling each element in the JSON payload
-            if (json != null) {
-                String sensor = (String) json.get("s");
-                System.out.println("PROVA1\n");
-                String ipv6 = ipAddress;
-                System.out.println("PROVA2\n");
-                JSONArray sensingType = (JSONArray) json.get("ss");
-                System.out.println("PROVA3 \n");
-                if (json.containsKey("t") && json.get("t") instanceof String) {
-                    String temporary = (String) json.get("t");
-                    // il resto del tuo codice
-                } else {
-                    System.out.println("Il valore di 't' non è una stringa o 't' non esiste");
-                }
-                if (sensor != null && ipv6 != null && sensingType != null ) {
-                    System.out.println("PROVA5\n");
-                    InetAddress addr = exchange.getSourceAddress();
-                    System.out.println("PROVA6\n");
-                    System.out.println("Source address: " + addr);
-                    try {
-                        // Insert the sensor IP in the database
-                        //checking if ip
-                        List<String> sensorIPs = db.getSensorIPs();
-                        if (sensorIPs.contains(addr.getHostAddress())) {
-                            System.err.println("Sensor IP already registered");
-                            response = new Response(CoAP.ResponseCode.BAD_REQUEST);
-                        }
-                        db.insertIPv6Address(addr.getHostAddress(), sensor);
-                        System.out.println("Sensor: " + sensor);
-                        System.out.println("IPv6: " + ipv6);
-                        System.out.println("Sensing Types: " + sensingType);
-                        System.out.println("Time Sample: " + timeSample);
 
-                        //insert sensor in the database
-                        IPv6DatabaseManager.createTableSensor(sensor, ipv6, sensingType, timeSample);
-                        /*if(sensor.equals("lpgsensor")) {
-                            db.insertSensorLPG(sensor, ipv6, sensingType, timeSample);
-                        } else if(sensor.equals("thermometer")){
-                            db.insertSensorTHERMOMETER(sensor, ipv6, sensingType, timeSample);
-                        }
-                        else
-                        {
-                            System.err.println("Missing required JSON keys");
-                            response = new Response(CoAP.ResponseCode.BAD_REQUEST);
-                        }*/
-                        response = new Response(CoAP.ResponseCode.CREATED);
-                        System.out.print(CoAP.ResponseCode.CREATED);
-                    } catch (Exception e) {
-                        System.err.println("Error inserting sensor IP in the database: " + e.getMessage());
-                        response = new Response(CoAP.ResponseCode.INTERNAL_SERVER_ERROR);
+        JSONParser parser = new JSONParser();
+        json = (JSONObject) parser.parse(payloadString);
+        System.out.println("Parsed JSON: " + json);
+        } catch (org.json.simple.parser.ParseException err) {
+            System.err.println("JSON parsing error: " + err.getMessage());
+            exchange.respond(CoAP.ResponseCode.BAD_REQUEST, "Invalid JSON format");
+            return;
+        }
+        Response response;
+        // Extracting and handling each element in the JSON payload
+        if (json != null) {
+            String sensor = (String) json.get("s");
+            String ipv6 = ipAddress;
+            JSONArray sensingType = (JSONArray) json.get("ss");
+            int timeSample = (int) json.get("t");
+            if (sensor != null && ipv6 != null && sensingType != null ) {
+                System.out.println("PROVA5\n");
+                InetAddress addr = exchange.getSourceAddress();
+                System.out.println("PROVA6\n");
+                System.out.println("Source address: " + addr);
+                try {
+                    // Insert the sensor IP in the database
+                    //checking if ip
+                    List<String> sensorIPs = db.getSensorIPs();
+                    if (sensorIPs.contains(addr.getHostAddress())) {
+                        System.err.println("Sensor IP already registered");
+                        response = new Response(CoAP.ResponseCode.BAD_REQUEST);
                     }
-                } else {
-                    System.err.println("Missing required JSON keys");
-                    response = new Response(CoAP.ResponseCode.BAD_REQUEST);
+                    db.insertIPv6Address(addr.getHostAddress(), sensor);
+                    System.out.println("Sensor: " + sensor);
+                    System.out.println("IPv6: " + ipv6);
+                    System.out.println("Sensing Types: " + sensingType);
+                    System.out.println("Time Sample: " + timeSample);
+
+                    //insert sensor in the database
+                    IPv6DatabaseManager.createTableSensor(sensor, ipv6, sensingType, timeSample);
+                    response = new Response(CoAP.ResponseCode.CREATED);
+                    System.out.print(CoAP.ResponseCode.CREATED);
+                } catch (Exception e) {
+                    System.err.println("Error inserting sensor IP in the database: " + e.getMessage());
+                    response = new Response(CoAP.ResponseCode.INTERNAL_SERVER_ERROR);
                 }
             } else {
+                System.err.println("Missing required JSON keys");
                 response = new Response(CoAP.ResponseCode.BAD_REQUEST);
             }
-
-            // Assigning values from JSON payload to variables
-            String sensor = (String) json.get("s");
-            String[] sensingTypes = (String[]) ((JSONArray) json.get("ss")).toArray(new String[0]);
-            long timeSample = (Long) json.get("t");
-
-            exchange.respond(response);
-            }
+        } else {
+            response = new Response(CoAP.ResponseCode.BAD_REQUEST);
+        }
     }
+}
