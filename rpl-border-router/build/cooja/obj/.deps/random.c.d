a2d5ca6fd5fbc4482f3e0b466b145a21b02b8f59
MenchiniDev <lorenzomenchini42@gmail.com>
2024-05-29 11:18:45 +0200

    fix conf

--- rpl-border-router/build/cooja/obj/.deps/random.c.d
+++
@@ -1,5 +0,0 @@
-build/cooja/obj/random.o: ../../../os/lib/random.c \
- ../../../os/services/rpl-border-router/module-macros.h \
- ../../../os/lib/random.h
-../../../os/services/rpl-border-router/module-macros.h:
-../../../os/lib/random.h:
